1e29524c7da8bb8d474f8f750b2ac60315552c1d
kobby <coleevans41@gmail.com>
2024-08-26 21:34:56 +0000

    merged homepage
    Co-authored-by: kobby-jewel <kobby-jewel@users.noreply.github.com>

--- lib/screen/home/home.dart
+++ lib/screen/home/home.dart
@@ -6,28 +6,32 @@ import 'package:lodge/screen/home/widget/custom_bottom_navigation.dart';
 import 'package:lodge/screen/home/widget/recommended_house.dart';
 import 'package:lodge/screen/home/widget/search_input.dart';
 import 'package:lodge/screen/home/widget/welcom_text.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
 
 class  HomePage extends StatelessWidget {
+  final String role;
+
+  HomePage({required this.role});
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-       backgroundColor: Theme.of(context).scaffoldBackgroundColor,
-       appBar:   CustomAppBar('Home'),
-      body:   SingleChildScrollView(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-           WelcomeText(),
-           SearchInput(onSearch: (query) {
-                 print('Search query: $query');
-              },),
-           Categories(),
-          RecommendedHouse(),
-          BestOffer(),
-        ],),
-      ),
-      bottomNavigationBar: CustomBottomNavigationBar(),
-    );
-  }
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _houseNameController = TextEditingController();
+  final TextEditingController _descriptionController = TextEditingController();
+  final TextEditingController _priceController = TextEditingController();
+  final TextEditingController _locationController = TextEditingController();
+  final TextEditingController _regionController = TextEditingController();
+  final TextEditingController _townController = TextEditingController();
+  final TextEditingController _countryController = TextEditingController();
+  final TextEditingController _gpsAddressController = TextEditingController();
+  final TextEditingController _apartmentTypeController = TextEditingController();
+  final TextEditingController _furnishingController = TextEditingController();
+  final TextEditingController _hController = TextEditingController();
+  final TextEditingController _houseNameController = TextEditingController();
+  final TextEditingController _houseNameController = TextEditingController();
+  final TextEditingController _houseNameController = TextEditingController();
+  final TextEditingController _houseNameController = TextEditingController();
 }
